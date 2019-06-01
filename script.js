aaa5f22745bb10ce59792f5abf505e6b7957b4c4
Dylan Sieren <dylan.sieren01@gmail.com>
2019-06-01 03:52:43 -0500

    added a footer in the html file, moved the menu structue around, made a series section to give the webapge some more content to allow scrolling, set up a sticky class for the navbar when the window is scrolled a certain threshold on the Y axis

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+// Global Variables
+const mainSearchLink = document.querySelector('.searchBarItem');
+const searchBarContainer = document.getElementById('searchContainer');
+// const bodyContainer = document.querySelector('body');
 
-const mainSearchLink = document.getElementById('mainSearchLink');
+// Event Listeners
+window.onscroll = () => {
+    toggleStickyNavbar();
+}
+// bodyContainer.addEventListener('click', clickOffSearch, false);
+
 mainSearchLink.addEventListener('click', toggleSearchBar, false);
 
 function toggleSearchBar() {
-    const searchBarContainer = document.getElementById('searchContainer');
-
     searchBarContainer.classList.toggle("on");
 
     if(searchBarContainer.classList.contains("on")=== true) {
         mainSearchLink.classList.add('activeState');
     } else if(searchBarContainer.classList.contains("on") === false) {
         mainSearchLink.classList.remove('activeState');
+    }
+}
+
+// function clickOffSearch(event) {
+//     const yPosition = event.clientY;
+//     if(searchBarContainer.classList.contains("on")=== true) {
+//         if(yPosition >= 263) {
+//             searchBarContainer.classList.remove('on');
+//             mainSearchLink.classList.remove('activeState');
+//         }
+//     }
+// }
+
+function toggleStickyNavbar() {
+    const mainNavigationContainer = document.getElementById('mainNavigation');
+    if(window.pageYOffset >= 100) {
+        mainNavigationContainer.classList.add("sticky");
+    } else if(window.pageYOffset <= 99) {
+        mainNavigationContainer.classList.remove("sticky");
     }
 }
